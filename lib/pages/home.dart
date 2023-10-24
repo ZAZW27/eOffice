3e81143608b9b213d5df2b38ddb7af825b35dc18
Zharif Aziz Zulkarnain WIdodo <zharifazizzulkarnain@gmail.com>
2023-10-24 14:16:43 +0800

    able to get four containers to pact together

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,10 +7,144 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: appBar(),
+    appBar: appBar(),
+      body: Column(
+        children: [
+          _bannerApp(context),
+          Expanded(
+            child: GridView.count(
+              crossAxisCount: 2, // 2 columns
+              children: <Widget>[
+                // CONTAINERS FOR GRID: 1 (top left)
+                Container(
+                  margin: EdgeInsets.all(5.0),
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                  ),
+                  child: Text('percobaan menggunakna container'),
+                  constraints: BoxConstraints(
+                    maxWidth: MediaQuery.of(context).size.width * 0.5, // Limit the width
+                    maxHeight: MediaQuery.of(context).size.height * 0.2, // Limit the height
+                  ),
+                ),
+                // CONTAINERS FOR GRID: 2 (top right)
+                Container(
+                  margin: EdgeInsets.all(5.0),
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                  ),
+                  child: Text('percobaan menggunakna container'),
+                  constraints: BoxConstraints(
+                    maxWidth: MediaQuery.of(context).size.width * 0.5, // Limit the width
+                    maxHeight: MediaQuery.of(context).size.height * 0.2, // Limit the height
+                  ),
+                ),
+                // CONTAINERS FOR GRID: 3 (bottom left)
+                Container(
+                  margin: EdgeInsets.all(5.0),
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 204, 218, 230),
+                  ),
+                  child: Text('percobaan menggunakna container'),
+                  constraints: BoxConstraints(
+                    maxWidth: MediaQuery.of(context).size.width * 0.5, // Limit the width
+                    maxHeight: MediaQuery.of(context).size.height * 0.3, // Limit the height
+                  ),
+                ),
+                // CONTAINERS FOR GRID: 4 (bottom right)
+                Container(
+                  margin: EdgeInsets.all(5.0),
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                  ),
+                  child: Text('percobaan menggunakna container'),
+                  constraints: BoxConstraints(
+                    maxWidth: MediaQuery.of(context).size.width * 0.5, // Limit the width
+                    maxHeight: MediaQuery.of(context).size.height * 0.2, // Limit the height
+                  ),
+                ),
+              ],
+            ),
+          )
+
+
+        ],
+      ),
     );
   }
 
+  /* =============================================================================
+  ----------------------------- CONTENT: BANNER APP ------------------------------
+  ============================================================================= */
+  Container _bannerApp(BuildContext context){
+    return Container(
+      // IMAGE BANNER
+      width: MediaQuery.of(context).size.height,
+      height: MediaQuery.of(context).size.height * 0.18, // 18% of screen height
+      margin: EdgeInsets.only(top: 10, left: 5, right: 5),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15), // Radius adjusment Match the other one below \/
+        image: DecorationImage(
+          image: AssetImage('assets/images/WavyBackground.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Stack(
+        children: [
+          // Gradient overlay
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15), // Match the radius of the outer container
+              gradient: LinearGradient(
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+                colors: [
+                  Colors.black, // Starting color (bottom)
+                  Colors.transparent, // Ending color (top)
+                ],
+              ),
+            ),
+          ),
+          // Content and texts
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center, // Center vertically
+            crossAxisAlignment: CrossAxisAlignment.start, // Align text to the left
+            children: [
+              Container(
+                margin: EdgeInsets.only(left: 20, right: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text( // First Text
+                      'PRANATA KOMPUTER PERTAMA',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text( // Second Text
+                      'Dinas Komunikasi dan Informatika Jabatan Definitif',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              // Add more Text widgets as needed
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  /* =============================================================================
+  ------------------------------- CONTENT: APP BAR -------------------------------
+  ============================================================================= */
   AppBar appBar() {
     return AppBar(
       // Title name for the app bar
